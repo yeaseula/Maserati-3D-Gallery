56d1277cfe3443fc1deabc46718ac0de11b18329
yeaseula <kminea@naver.com>
2025-09-18 01:48:00 +0900

    판넬, 박스 메쉬 컬러 변경 및 배경 이미지 추가

--- pages/Showroom.jsx
+++ pages/Showroom.jsx
@@ -14,7 +14,7 @@ const modelMap = {
         position: [0,-0.75,0],
         scale:[100,100,100],
         rotation:[0,(-Math.PI / 2) + 1.45, 0],
-        lightpower:10
+        lightpower:20
     }
 }
 
@@ -24,7 +24,7 @@ function ProductCall({modalPath,position,scale,rotation,colors}) {
     useMemo(() => {
         gltf.scene.traverse((child) => {
             if (!child.isMesh) return;
-            console.log(child.name)
+            // console.log(child.name)
             const meshname = child.name.toLowerCase();
             if(meshname.includes('hood') || (meshname.includes('door') && meshname.includes('levante')) ||
             (meshname.includes('rear') && meshname.includes('004')) || (meshname.includes('frontkit') && meshname.includes('gts_001'))){
@@ -58,7 +58,7 @@ function ProductCall({modalPath,position,scale,rotation,colors}) {
 
 function MyScene({...props}) {
     const spotLightRef = useRef();
-    useHelper(spotLightRef, SpotLightHelper, 'cyan'); // The third argument is the helper's color
+    //useHelper(spotLightRef, SpotLightHelper, 'cyan'); // The third argument is the helper's color
     return (
         <spotLight
             ref={spotLightRef}
@@ -74,7 +74,7 @@ function MyScene({...props}) {
 }
 function MyScene2({...props}) {
     const spotLightRef = useRef();
-    useHelper(spotLightRef, SpotLightHelper, 'cyan'); // The third argument is the helper's color
+    //useHelper(spotLightRef, SpotLightHelper, 'red'); // The third argument is the helper's color
     return (
         <spotLight
             ref={spotLightRef}
@@ -93,8 +93,8 @@ function Window(){
     const treeTexture = useTexture('/src/assets/images/tree-background.jpg')
     return(
         <mesh rotation-y={(Math.PI / 2)} position={[-1.9,0.7,0]}>
-            <planeGeometry args={[5,2]}/>
-            <meshBasicMaterial map={treeTexture}/>
+            <planeGeometry args={[5.6,2.6]}/>
+            <meshBasicMaterial map={treeTexture} color={'white'}/>
         </mesh>
     )
 }
@@ -108,14 +108,15 @@ export default function Showroom({product}) {
     return (
         <div className="w-[100vw] h-[100vh]">
             <Canvas shadows
-            camera={{ position:[5,1,5], fov:45 }}
+            camera={{ position:[5,1,5], fov:50 }}
             key={product}
             className="w-[100vw] h-[100vh]"
             >
                 <color attach="background" args={['#fafafa']} />
-                <ambientLight intensity={0.6} color={'0xffffff'}></ambientLight>
-                <MyScene2 color="gold" position={[-5,5,-5]} intensity={120}></MyScene2>
-                <MyScene color="gold" position={[4, 4, 4]} intensity={LightPower}></MyScene>
+                <ambientLight intensity={2} color={'white'}></ambientLight>
+                <MyScene2 color="#FFDDA0" position={[-5,5,-5]} intensity={50}></MyScene2>
+                <MyScene color="#FFDDA0" position={[4, 4, 4]} intensity={LightPower}></MyScene>
+                <directionalLight color="#ffffff" intensity={1} position={[5,10,3]} castShadow />
                 <Suspense fallback={null}>
                     <ProductCall
                     modalPath={modalPath.modalPath}
@@ -125,7 +126,7 @@ export default function Showroom({product}) {
                     colors={colors}
                     ></ProductCall>
                     <Environment
-                        files="/src/assets/hdr/artist_workshop.hdr"
+                        files="/src/assets/hdr/tree-background.hdr"
                         background={false}
                         path=""
                         preset={null}
@@ -134,16 +135,46 @@ export default function Showroom({product}) {
                 /* 옆면 벽 */
                 <mesh rotation-x={Math.PI} rotation-y={(Math.PI / 2)} position={[-2,0.7,0]}>
                     <planeGeometry args={[6,3]}/>
+                    <meshStandardMaterial
+                        color={'#F5EAD7'}
+                        metalness={0.6}
+                    />
                 </mesh>
                 <Window/>
+                <mesh rotation-y={(Math.PI / 2)} position={[-1.87,0.7,0]}>
+                    <boxGeometry args={[5.6,2.6,0]} />
+                    <meshPhysicalMaterial
+                        color="white"
+                        transparent
+                        opacity={0.15}
+                        transmission={1}
+                        metalness={0.3}
+                        roughness={0.05}
+                        reflectivity={1}
+                        clearcoat={1}
+                        clearcoatRoughness={0}
+                        ior={1.5}
+                        thickness={0.05}
+                    />
+                </mesh>
+                <mesh rotation-y={(Math.PI / 2)} position={[-1.88,0.7,2.8]}>
+                    <boxGeometry args={[0.12,2.6,0.04]}/>
+                    <meshBasicMaterial
+                        color="#ffffff"
+                    />
+                </mesh>
                 /* 뒷면 벽 */
                 <mesh position={[0,0.7,-3]}>
                     <planeGeometry args={[4,3]}/>
+                    <meshStandardMaterial
+                        color={'#F5EAD7'}
+                        metalness={0}
+                    />
                 </mesh>
                 <mesh rotation-x={-Math.PI / 2} position={[0,-0.8,0]} receiveShadow>
                     <planeGeometry args={[4, 6]} />
                     /* 하나의 메쉬에 하나의 머터리얼만 가능 */
-                    <meshStandardMaterial color="#757575" roughness={0.01} metalness={0.3} />
+                    <meshStandardMaterial color="#E6D8AD" roughness={0.01} metalness={0.3} />
 
                 </mesh>
                 <OrbitControls></OrbitControls>
