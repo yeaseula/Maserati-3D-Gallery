26e9d45ca65f02f3f1fd3fa087caf98c8c4ada91
yeaseula <kminea@naver.com>
2025-09-27 21:54:56 +0900

    ✅ Test - traverse와 성능 관계 테스트

--- components/canvas/CarModel.jsx
+++ components/canvas/CarModel.jsx
@@ -9,52 +9,52 @@ export default function CarModel({modalPath,colors,calliper}) {
     function ProductCall({modalPath,position,scale,rotation,colors,calliper}) {
         const gltf = useGLTF(`/glb/optimized/${modalPath}`, true);
 
-        useMemo(() => {
-            gltf.scene.traverse((child) => {
-                if (!child.isMesh) return;
-                //console.log(child.name)
-                const meshname = child.name.toLowerCase();
-                if(meshname.includes('hood') || (meshname.includes('door') && meshname.includes('levante')) ||
-                (meshname.includes('rear') && meshname.includes('004')) || (meshname.includes('frontkit') && meshname.includes('gts_001'))){
-                    child.material = new THREE.MeshStandardMaterial({
-                        color:colors,
-                        metalness: 0.2,
-                        roughness: 0.1,
-                    });
-                }
-                if(meshname.includes('glass') || meshname.includes('window')){
-                    child.material = new THREE.MeshPhysicalMaterial({
-                        color:'white',
-                        metalness:0,
-                        roughness:0,
-                        clearcoat:1,
-                        clearcoatRoughness:0.2,
-                        transmission:1,
-                        reflectivity:1,
-                        opacity:0.25,
-                        transparent:0,
-                        thickness:0.3,
-                        ior:1.15,
-                    })
-                }
-                //cielo 모델
-                if(meshname.includes('lodabody')){
-                    child.material = new THREE.MeshStandardMaterial({
-                        color:colors,
-                        metalness: 0.2,
-                        roughness: 0.1,
-                    });
-                }
-                if(meshname.includes('calliper') || meshname.includes('caliper')){
-                    child.material = new THREE.MeshStandardMaterial({
-                        color:calliper
-                    })
-                }
+        // useMemo(() => {
+        //     gltf.scene.traverse((child) => {
+        //         if (!child.isMesh) return;
+        //         //console.log(child.name)
+        //         const meshname = child.name.toLowerCase();
+        //         if(meshname.includes('hood') || (meshname.includes('door') && meshname.includes('levante')) ||
+        //         (meshname.includes('rear') && meshname.includes('004')) || (meshname.includes('frontkit') && meshname.includes('gts_001'))){
+        //             child.material = new THREE.MeshStandardMaterial({
+        //                 color:colors,
+        //                 metalness: 0.2,
+        //                 roughness: 0.1,
+        //             });
+        //         }
+        //         if(meshname.includes('glass') || meshname.includes('window')){
+        //             child.material = new THREE.MeshPhysicalMaterial({
+        //                 color:'white',
+        //                 metalness:0,
+        //                 roughness:0,
+        //                 clearcoat:1,
+        //                 clearcoatRoughness:0.2,
+        //                 transmission:1,
+        //                 reflectivity:1,
+        //                 opacity:0.25,
+        //                 transparent:0,
+        //                 thickness:0.3,
+        //                 ior:1.15,
+        //             })
+        //         }
+        //         //cielo 모델
+        //         if(meshname.includes('lodabody')){
+        //             child.material = new THREE.MeshStandardMaterial({
+        //                 color:colors,
+        //                 metalness: 0.2,
+        //                 roughness: 0.1,
+        //             });
+        //         }
+        //         if(meshname.includes('calliper') || meshname.includes('caliper')){
+        //             child.material = new THREE.MeshStandardMaterial({
+        //                 color:calliper
+        //             })
+        //         }
 
-                child.castShadow = true;
-                child.receiveShadow = false;
-            });
-        }, [gltf,colors,calliper]);
+        //         child.castShadow = true;
+        //         child.receiveShadow = false;
+        //     });
+        // }, [gltf,colors,calliper]);
 
         return (
             <group scale={scale} position={position} rotation={rotation}>
